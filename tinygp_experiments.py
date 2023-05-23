0871f80dae992fc9491b665931172ae5c8d53037
Balduin Dettling <balduindettling@gmail.com>
2023-05-23 11:38:10 +0200

    multivariate example

--- tinygp_experiments.py
+++ tinygp_experiments.py
@@ -54,12 +54,10 @@ class DerivativeKernel(tinygp.kernels.Kernel):
         )
 
 
-
-if __name__ == '__main__':
-
+def example_univariate():
     # make samples of some function and its gradient
 
-    noise_size = .05
+    noise_size = 1e-4
 
     def f(x):
         return np.sin(x) + x**2/100
@@ -157,5 +155,118 @@ if __name__ == '__main__':
 
 
     # next experiment: the same plus derivative info....
-    # also multivariate data.
     ipdb.set_trace()
+
+def example_multivariate():
+
+    # multivariate seems easy, it kind of just works
+
+    key = jax.random.PRNGKey(0)
+
+    N_pts = 256
+    nx = 2
+
+    noise_size = 1e-4
+
+    key, subkey = jax.random.split(key)
+    xs_f = jax.random.normal(subkey, (N_pts, nx))
+
+    def f(x):
+        return x[0]**2 + (x[1]-x[0]**2)**2
+
+    key, subkey = jax.random.split(key)
+    fs = jax.vmap(f)(xs_f) + noise_size * jax.random.normal(subkey, (N_pts,))
+
+
+    def build_gp(params, X):
+        # exp transform for >0 constraint.
+        amp = np.exp(params['log_amp'])
+        scale_vec = np.exp(params['log_scale'])
+
+        # ...just the kernel specification syntax took some getting used to
+        # linear will first scale by the diagonal matrix diag(scale_vec),
+        # then pass that to the kernel, ExpSquared.
+        kernel = amp * tinygp.transforms.Linear(
+                scale_vec, tinygp.kernels.ExpSquared()
+        )
+
+        # small noise of same shape as x variable.
+        diag = 1e-3 * np.ones_like(X)
+        return tinygp.GaussianProcess(kernel, X, diag=noise_size**2)
+
+
+    def nll(params, X, y):
+        gp = build_gp(params, X)
+        return -gp.log_probability(y)
+
+
+    # no mean anymore.
+    params = {
+        'log_amp': np.log(1),
+        'log_scale': np.zeros(nx),
+    }
+
+    nll_value_grad = jax.jit(jax.value_and_grad(nll, argnums=0))
+
+    v, g = nll_value_grad(params, xs_f, fs)
+    print(v)
+    print(g)
+
+    # set up optimiser for neg log likelihood
+    opti = optax.adam(learning_rate=.02)
+    opt_state = opti.init(params)
+
+    losses = []
+    for i in range(500):
+        loss_val, grads = nll_value_grad(params, xs_f, fs)
+        losses.append(loss_val)
+        updates, opt_state = opti.update(grads, opt_state)
+        params = optax.apply_updates(params, updates)
+
+    v, g = nll_value_grad(params, xs_f, fs)
+    print(v)
+    print(g)
+    pl.plot(losses)
+    pl.show()
+
+
+    # do the inference, make a plot.
+    trained_gp = build_gp(params, xs_f)
+
+    extent = 3
+    x_grid, y_grid = np.linspace(-extent, extent, 50), np.linspace(-extent, extent, 50)
+    x_, y_ = np.meshgrid(x_grid, y_grid)
+    X_pred = np.vstack((x_.flatten(), y_.flatten())).T
+    y_true = jax.vmap(f)(X_pred).reshape(x_.shape)
+
+    pred_gp = trained_gp.condition(fs, X_pred).gp
+    y_pred = pred_gp.loc.reshape(y_true.shape)
+    y_std = np.sqrt(pred_gp.variance.reshape(y_true.shape))
+
+    fig, axs = pl.subplots(1, 2, figsize=(12, 6), sharex=True, sharey=True)
+    axs[0].pcolor(x_, y_, y_pred, vmin=y_true.min(), vmax=y_true.max())
+    axs[0].scatter(
+            xs_f[:, 0], xs_f[:, 1],
+            c=fs, ec='black',
+            vmin=y_true.min(), vmax=y_true.max()
+    )
+
+    y_std = np.log(y_std)
+    axs[1].pcolor(x_, y_, y_std, cmap='plasma', vmin=y_std.min(), vmax=y_std.max())
+    pl.show()
+
+
+
+
+    ipdb.set_trace()
+
+
+    # make
+
+
+if __name__ == '__main__':
+
+    # example_univariate()
+    example_multivariate()
+
+

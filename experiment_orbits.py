e9b222717d624fc9ef0fcbd61a085f059f8c5367
mbjd <balduindettling@gmail.com>
2023-11-24 17:25:05 +0100

    nicer visualisations for orbits example
    value level sets are really easy to plot if you integrate
    characteristics with value as independent variable :) but still ofc
    they intersect (level sets)/overlap (sublevel sets).

--- experiment_orbits.py
+++ experiment_orbits.py
@@ -130,7 +130,7 @@ x0s = 0.01 * np.linalg.inv(scipy.linalg.sqrtm(P0_inf)) @ circle_xs + xf[:, None]
 
 x0 = x0s[:, 0]
 v0 = (lambda x: (x-xf).T @ P0_inf @ (x-xf))(x0)
-v1 = 8
+v1 = 4
 
 # test single solution first
 
@@ -138,60 +138,132 @@ lam0 = jax.jacobian(lambda x: (x-xf).T @ P0_inf @ (x-xf))(x0)
 y0 = np.concatenate([x0, lam0])
 # sol = pontryagin_solver(y0, v0, v1)
 
-def get_sol(theta):
-    direction = np.array([np.sin(theta), np.cos(theta)])
-    x0 = 0.01 * np.linalg.inv(scipy.linalg.sqrtm(P0_inf)) @ direction + xf
-    lam0 = jax.jacobian(lambda x: (x-xf).T @ P0_inf @ (x-xf))(x0)
-    y0 = np.concatenate([x0, lam0])
-    sol = pontryagin_solver(y0, v0, v1)
-    return sol
-
-@jax.jit
-def sol_single_and_dxdtheta(theta):
-
+load=False
+if not load:
     def get_sol(theta):
         direction = np.array([np.sin(theta), np.cos(theta)])
         x0 = 0.01 * np.linalg.inv(scipy.linalg.sqrtm(P0_inf)) @ direction + xf
-        # lam0 = jax.jacobian(lambda x: (x-xf).T @ P0_inf @ (x-xf))(x0)
-        lam0 = 2 * P0_inf @ (x0-xf)
+        lam0 = jax.jacobian(lambda x: (x-xf).T @ P0_inf @ (x-xf))(x0)
         y0 = np.concatenate([x0, lam0])
         sol = pontryagin_solver(y0, v0, v1)
         return sol
 
-    get_dsol = jax.jacobian(get_sol)
+    @jax.jit
+    def sol_single_and_dxdtheta(theta):
 
-    # finite difference = bad they said
-    dtheta = .00001
-    sol = get_sol(theta)
-    solp = get_sol(theta + dtheta)
+        def get_sol(theta):
+            direction = np.array([np.sin(theta), np.cos(theta)])
+            x0 = 0.01 * np.linalg.inv(scipy.linalg.sqrtm(P0_inf)) @ direction + xf
+            # lam0 = jax.jacobian(lambda x: (x-xf).T @ P0_inf @ (x-xf))(x0)
+            lam0 = 2 * P0_inf @ (x0-xf)
+            y0 = np.concatenate([x0, lam0])
+            sol = pontryagin_solver(y0, v0, v1)
+            return sol
 
-    dsol_final = (solp.evaluate(v1) - sol.evaluate(v1))/dtheta
+        get_dsol = jax.jacobian(get_sol)
 
-    vs = np.linspace(np.sqrt(v0), np.sqrt(v1), 51)**2
-    ys = jax.vmap(sol.evaluate)(vs)
-    return vs, ys, dsol_final
+        # finite difference = bad they said
+        dtheta = .00001
+        sol = get_sol(theta)
+        solp = get_sol(theta + dtheta)
 
-sol_single_and_dxdtheta(.1)
+        dsol_final = (solp.evaluate(v1) - sol.evaluate(v1))/dtheta
 
-ax = pl.figure().add_subplot(projection='3d')
-    # ax.plot(ys[:, :, 0].reshape(-1), ys[:, :, 1].reshape(-1), vs[:, :].reshape(-1), color='green', alpha=.5)
+        vs = np.linspace(np.sqrt(v0), np.sqrt(v1), 41)**2
+        ys = jax.vmap(sol.evaluate)(vs)
+        return vs, ys, dsol_final
 
-theta = 0
-target_x_steplen = .5
-while theta < 2*np.pi:
-    print(theta)
-    ts, ys, dfinaldtheta = sol_single_and_dxdtheta(theta)
+    sol_single_and_dxdtheta(.1)
 
-    cmap = matplotlib.colormaps['hsv']
-    ax.plot(ys[:, 0].reshape(-1), ys[:, 1].reshape(-1), ts.reshape(-1), color='green', alpha=.1)
-    # pl.plot(ys[:, 0], ys[:, 1], color='green', alpha=.1)
+    ax = pl.figure().add_subplot(projection='3d')
+    ax2d = pl.figure().add_subplot()
+        # ax.plot(ys[:, :, 0].reshape(-1), ys[:, :, 1].reshape(-1), vs[:, :].reshape(-1), color='green', alpha=.5)
 
-    dx_final = dfinaldtheta[0:2]
-    steplen_per_theta = np.linalg.norm(dfinaldtheta)
-    thetastep = target_x_steplen / steplen_per_theta
-    theta = theta + thetastep
+    theta = 0
+    target_x_steplen = .2
+    all_vs = []
+    all_ys = []
 
+    i=0
+    while theta < 2*np.pi:
+        i = i+1
+        print(theta)
+        ts, ys, dfinaldtheta = sol_single_and_dxdtheta(theta)
+
+        all_vs.append(ts)
+        all_ys.append(ys)
+
+        cmap = matplotlib.colormaps['hsv']
+        # ax.plot(ys[:, 0].reshape(-1), ys[:, 1].reshape(-1), ts.reshape(-1), color='green', alpha=.1)
+
+        if i % 40 == 0:
+            ax2d.plot(ys[:, 0], ys[:, 1], color='black', alpha=.3)
+            ax.plot(ys[:, 0], ys[:, 1], ts, color='black', alpha=.3)
+
+        dx_final = dfinaldtheta[0:2]
+        steplen_per_theta = np.linalg.norm(dfinaldtheta)
+        thetastep = target_x_steplen / steplen_per_theta
+        theta = theta + thetastep
+
+
+    all_ys = np.array(all_ys)
+    all_vs = np.array(all_vs)
+
+    # make basically the same plot, but with the data transposed, so we plot value level sets
+    # instead of trajectories.
+    # ax = pl.figure().add_subplot(projection='3d')
+    # for each value level set:
+    for vlevel in range(all_vs.shape[1]):
+        vvec = all_vs[:, vlevel]
+        x0vec = all_ys[:, vlevel, 0]
+        x1vec = all_ys[:, vlevel, 1]
+
+        #
+        ax2d.plot(x0vec, x1vec, color=matplotlib.colormaps['jet'](vvec[0]/v1), alpha=.3)
+        ax.plot(x0vec, x1vec, vvec, color=matplotlib.colormaps['jet'](vvec[0]/v1), alpha=.3)
+
+    thetas = np.linspace(0, 2*np.pi, 501)
+    ax2d.plot(np.sin(thetas), np.cos(thetas), color='black')
+    ax.plot(np.sin(thetas), np.cos(thetas), 0 * thetas, color='black')
+    ax2d.scatter([0], [1], [0], color='black')
+
+
+    pl.show()
+    ipdb.set_trace()
+
+    # np.save('ys', all_ys)
+    # np.save('vs', all_vs)
+
+else:
+    all_ys = np.load('ys')
+    all_vs = np.load('vs')
+
+extent = 1.5
+N_linspace = 81
+x1s = x2s = np.linspace(-extent, extent, N_linspace)
+vs = onp.ones((N_linspace, N_linspace)) * v1
+
+# map float between -extent and extent to int from 0 to N_linspace-1
+state_to_idx = lambda x: int((x + extent) / (2*extent) * (N_linspace-1))
+
+for v, ys in tqdm.tqdm(zip(all_vs, all_ys), total=all_vs.shape[0]):
+    try:
+        idx_x1 = state_to_idx(ys[0])
+        idx_x2 = state_to_idx(ys[1])
+        if vs[idx_x1, idx_x2] >= v:
+            vs[idx_x1, idx_x2] = v
+    except:
+        # either ys is nan somewhere, or indices out of bounds.
+        pass
+
+pl.imshow(vs[:, ::-1].T, extent=(-extent, extent, -extent, extent), aspect='equal')
+thetas = np.linspace(0, 2*np.pi, 501)
+pl.plot(np.sin(thetas), np.cos(thetas))
+pl.scatter([0], [1], color='red')
 pl.show()
+
+
+ipdb.set_trace()
 
 @jax.jit
 def x0_to_vs_ys(x0):

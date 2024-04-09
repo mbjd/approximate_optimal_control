f4cdc778f77fcad9476cecfda4697f5841023c65
mbjd <balduindettling@gmail.com>
2024-04-09 17:41:16 +0200

    extra fancy file sync

---
+++ pull_euler.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+# how tf was the syntax with and without trailing "/" again?
+
+rsync -av dbalduin@euler.ethz.ch:/cluster/home/dbalduin/approximate_optimal_control/tmp ~/approximate_optimal_control/tmp_euler

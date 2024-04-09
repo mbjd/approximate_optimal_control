fc84ce2d6a957632166256f4fb5ddd3fe2b631ff
mbjd <balduindettling@gmail.com>
2024-04-09 17:06:05 +0200

    oops wrong launch command (?)

--- launch.sh
+++ launch.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-#SBATCH -n 1
-#SBATCH --cpus-per-task=1
-#SBATCH --time=1:00:00
-#SBATCH --job-name="flatquad_test"
-#SBATCH --mem-per-cpu=1024
-#SBATCH --output="./flatquad_out.txt"
-#SBATCH --error="./flatquad_err.txt"
-#SBATCH --open-mode=append
-
-./flatquad_landing_experiment.py
+sbatch -n 1 --cpus-per-task=1 --time=2:00:00 --mem-per-cpu=8192 --output="./tmp/flatquad_output.txt" --error="./tmp/flatquad_err.txt" --wrap="./flatquad_landing_experiment.py"

a28a61b4f434700527ea93b3be74e7d33776d799
mbjd <balduindettling@gmail.com>
2024-06-04 15:00:18 +0200

    rudimentary controlcost plots for flatquad.

--- flatquad_result_fig.py
+++ flatquad_result_fig.py
@@ -23,7 +23,7 @@ import tqdm
 import pontryagin_utils
 from fig_config import *
 from misc import *
-from flatquad_experiment import base_algo_params, define_problem_params
+from flatquad_landing_experiment import base_algo_params, define_problem_params
 
 # make flatquad plots.
 # step 1: make data with levelsets.evaluate. write all sorts of control
@@ -31,6 +31,24 @@ from flatquad_experiment import base_algo_params, define_problem_params
 # step 2: read data here, make plots. do as little extra computation as
 # possible.
 
-run_id = '133742069'
+run_id = 'mo8ys11a'
+run_id = 'uqf3ybp8'
 
-# TODO the rest
+# controlcosts common.
+
+fpath = os.path.join(data_dir, f'flatquad_{run_id}_controlcosts_common.msgpack.gz')
+with gzip.open(fpath, 'rb') as f:
+    bs = f.read()
+eval_outputs = flax.serialization.msgpack_restore(bs)
+eval_outputs = jtm(np.array, eval_outputs)  # np array -> jax array
+
+pl.figure('controlcost vs v_mean')
+ipdb.set_trace()
+costs = eval_outputs['costs']
+pl.loglog(eval_outputs['v_mean'], costs, '. ')
+
+pl.figure('controlcost cdf')
+pl.semilogx((costs/eval_outputs['v_mean']).sort(), np.linspace(0, 1, costs.shape[0]))
+pl.show()
+
+ipdb.set_trace()
